d147838e4cb691fe4f6a22714846346416c3c46f
lucascs20182 <lucascs20182@gmail.com>
2021-01-10 07:53:57 -0300

    XMLHttpRequest

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
+const multer = require('multer')
+
 const app = express()
 
 // serve o conteúdo estático (html, js, fontes etc.)
@@ -12,5 +14,26 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // transforma json em objeto
 app.use(bodyParser.json())
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function(req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if(err) {
+            return res.end('erro')
+        }
+
+        res.end('concluído com sucesso')
+    })
+})
 
 app.listen(8080, () => console.log('executando'))
